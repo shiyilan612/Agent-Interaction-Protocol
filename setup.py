c78db1ea33f925361e2d3f4b5a8d66cbcab8358e
clleng <7521454+clleng@user.noreply.gitee.com>
2025-06-12 13:51:58 +0800

    init

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.0.1",
     packages=find_packages(),
     install_requires=["grpcio", "grpcio-tools", "tinydb", "aiohttp", "colorlog"],
-    author="Haixin Wang, Xiaomian Kang, Chunlinm Leng",
+    author="Haixin Wang, Xiaomian Kang, Chunlin Leng",
     author_email="{haixin.wang & xiaomian.kang}@nlpr.ia.ac.cn, lengchunlin2023@ia.ac.cn",
     description="Distributed Interconnected Agent Communication Protocol",
     python_requires=">=3.10"
