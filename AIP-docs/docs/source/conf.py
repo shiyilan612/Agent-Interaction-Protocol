596d0423b0aaedf60f351d8246877b5a311fc918
shiyilan <18547153645@163.com>
2025-09-09 19:11:11 +0800

    Add conf.py for ReadTheDocs

---
+++ AIP-docs/docs/source/conf.py
@@ -0,0 +1,12 @@
+project = "AgentInteractionProtocol"
+author = "shiyilan"
+extensions = ["myst_parser"]  # 支持Markdown解析
+exclude_patterns = ["_build"]
+html_theme = "sphinx_rtd_theme"
+
+# 支持中文
+language = "en"
+locale_dirs = ["locale/"]  # 翻译文件目录
+gettext_compact = False
+
+html_dir = '../docs'
